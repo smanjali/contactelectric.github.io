51ad8830b7e15dd703ce3d3f377339767bd9af5f
smanjali <anjaliriedel@gmail.com>
2026-06-19 18:15:37 -0600

    did a whole bunch of about page edits

--- menu.js
+++ menu.js
@@ -7,3 +7,18 @@ function closeMenu() {
 }
 
 console.log(document.getElementById("current-year").textContent = new Date().getFullYear());
+
+var collapse = document.getElementsByClassName("collapser");
+var i;
+
+for (i = 0; i < collapse.length; i++) {
+    collapse[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}   
